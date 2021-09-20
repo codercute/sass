9b3ad7235013a899a73b7e1cee84a54c75feef07
zzh <1371252281@qq.com>
2021-09-20 13:21:17 +0800

    提交

---
+++ src/directives/index.js
@@ -0,0 +1,9 @@
+import { done } from "nprogress"
+
+export const imageError = {
+    inserted(dom, options) {
+        dom.onerror = function(){
+            dom.src = options.value
+        }
+    }
+}
--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
-
+import * as directives from '@/directives'
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -23,10 +23,10 @@ import '@/permission' // permission control
  * Currently MockJs will be used in the production environment,
  * please remove it before going online ! ! !
  */
-if (process.env.NODE_ENV === 'production') {
-  const { mockXHR } = require('../mock')
-  mockXHR()
-}
+// if (process.env.NODE_ENV === 'production') {
+//   const { mockXHR } = require('../mock')
+//   mockXHR()
+// }
 
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
@@ -34,6 +34,10 @@ Vue.use(ElementUI, { locale })
 // Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+Object.keys(directives).forEach(key => {
+  Vue.directive(key, directives[key])
+})
 
 new Vue({
   el: '#app',
